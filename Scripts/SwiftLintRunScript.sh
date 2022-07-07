729306119f9d465e2815a50704e461a9a94487b6
baegteun <baegteun@gmail.com>
2022-07-07 23:03:11 +0900

    :memo: :: SwiftLint run script

--- Scripts/SwiftLintRunScript.sh
+++ Scripts/SwiftLintRunScript.sh
@@ -1,5 +1,11 @@
+if test -d "/opt/homebrew/bin/"; then
+	PATH="/opt/homebrew/bin/:${PATH}"
+fi
+
+export PATH
+
 if which swiftlint > /dev/null; then
-  swiftlint
+	swiftlint
 else
-  echo  "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+	echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
