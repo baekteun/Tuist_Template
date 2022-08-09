c379b94153c9dcf593cf52b00c4b8f20878ab97b
baegteun <baegteun@gmail.com>
2022-08-09 19:24:45 +0900

    :sparkles: :: Demo 생성 script

--- Scripts/generate_new_feature.py
+++ Scripts/generate_new_feature.py
@@ -8,6 +8,8 @@ def make_new_feature(feature_name, has_demo=False):
     make_project_file(feature_name, f"{feature_name}Feature", has_demo)
     make_sources(feature_name)
     make_tests(feature_name)
+    if has_demo:
+        make_demo(feature_name)
 
 def write_code_in_file(file_path, codes):
     if not os.path.isfile(file_path):
@@ -71,6 +73,64 @@ class TargetTests: XCTestCase {
 }
 '''
     write_code_in_file(test_file_path, test_content)
+
+def make_demo(feature_name):
+    make_dir(f'{feature_name}Feature/Demo')
+    make_dir(f'{feature_name}Feature/Demo/Sources')
+    make_dir(f'{feature_name}Feature/Demo/Resources')
+    launch_path = f'{feature_name}Feature/Demo/Resources/LaunchScreen.storyboard'
+    launch = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="13122.16" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" launchScreen="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES" initialViewController="01J-lp-oVM">
+    <dependencies>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="13104.12"/>
+        <capability name="Safe area layout guides" minToolsVersion="9.0"/>
+        <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
+    </dependencies>
+    <scenes>
+        <!--View Controller-->
+        <scene sceneID="EHf-IW-A2E">
+            <objects>
+                <viewController id="01J-lp-oVM" sceneMemberID="viewController">
+                    <view key="view" contentMode="scaleToFill" id="Ze5-6b-2t3">
+                        <rect key="frame" x="0.0" y="0.0" width="375" height="667"/>
+                        <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
+                        <color key="backgroundColor" xcode11CocoaTouchSystemColor="systemBackgroundColor" cocoaTouchSystemColor="whiteColor"/>
+                        <viewLayoutGuide key="safeArea" id="6Tk-OE-BBY"/>
+                    </view>
+                </viewController>
+                <placeholder placeholderIdentifier="IBFirstResponder" id="iYj-Kq-Ea1" userLabel="First Responder" sceneMemberID="firstResponder"/>
+            </objects>
+            <point key="canvasLocation" x="53" y="375"/>
+        </scene>
+    </scenes>
+</document>
+'''
+    write_code_in_file(launch_path, launch)
+
+    app_delegate_path = f'{feature_name}Feature/Demo/Sources/AppDelegate.swift'
+    app_delegate = '''import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .yellow
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+'''
+    write_code_in_file(app_delegate_path, app_delegate)
+
 
 
 
