226073176dc309d2bd3d5acfd257da997116ab6b
baegteun <74440939+baekteun@users.noreply.github.com>
2022-08-11 12:25:14 +0900

    Update Environment.swift

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -4,7 +4,7 @@ public enum Environment {
     public static let appName = ""
     public static let targetName = ""
     public static let targetTestName = "\(targetName)Tests"
-    public static let organizationName = "baegteun"
+    public static let organizationName = ""
     public static let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "13.0", devices: [.iphone, .ipad])
     public static let platform = Platform.iOS
     public static let baseSetting: SettingsDictionary = SettingsDictionary()
